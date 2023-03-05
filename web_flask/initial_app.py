fdca12694f7628c43030bf60d358631c5cd82524
George Dagg <74677961+georgedagg@users.noreply.github.com>
2023-03-05 14:35:07 +0000

    incorporate html and css pages with flask

--- web_flask/initial_app.py
+++ web_flask/initial_app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
+print(app.static_folder)
 
 @app.route('/')
-def connect_to_db():
-    a=3
-    b=5
-    c=a+b
-    return "Hi"
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+    
+
 
 
 
