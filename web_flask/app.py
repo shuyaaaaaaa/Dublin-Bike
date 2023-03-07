0ccbe4ded7e339dbe1ad12fc89f26893eff618df
George Dagg <74677961+georgedagg@users.noreply.github.com>
2023-03-07 11:53:11 +0000

    added live hover details

--- web_flask/app.py
+++ web_flask/app.py
@@ -91,5 +91,50 @@ def index():
     else:
         return render_template('index.html', data=json.dumps(data))
 
+@app.route('/hover', methods=['GET', 'POST'])
+def hover():
+    # ------ Station Information - Marker Hover ------- 
+
+    # If POST request submitted:
+    if request.method == 'POST':
+        try:
+        # Connect to JCDecaux API
+            r = requests.get(login.jcdUri, params={'contract':login.jcdName,
+                                            'apiKey': login.jcdKey})
+
+            if r.status_code == 200:
+                # If connection successful:
+                print('Connection to JCDecaux successful!')
+                data = r.json()
+
+                # Extract station number POST request: 
+                user_request = int(request.form['station_num'])
+                print('Extracted station number =', user_request)
+                
+                # For each station, extract the following data points:
+                for station in data:
+                    station_num = station.get('number')
+                    if station_num == user_request:
+                        available_bike_stands = station.get('available_bike_stands')
+                        available_bikes = station.get('available_bikes')
+                        break
+                
+                # Response HTML for POST request
+                text_response = f"<p>Bikes: {available_bikes} Stands: {available_bike_stands}</p>"
+                    
+                try:
+                    # Render the HTML with the extracted data points
+                    return text_response
+                except Exception as e:
+                    print('Error displaying web page. Error: ', e)
+                    
+            else:
+                # If connection to JCDecaux fails, render an error message
+                error_response = "<h4>Something went wrong. Please try again.</h4>"
+                return error_response
+
+        except Exception as e:
+            print("Connection error:", e)
+
 if __name__ == '__main__':
     app.run(debug=True)
