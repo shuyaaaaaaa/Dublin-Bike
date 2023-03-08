0d2624dc9b6676a3f8008763fb1f5706b76a64d6
George Dagg <74677961+georgedagg@users.noreply.github.com>
2023-03-08 17:20:20 +0000

    incorporated cian's route planner- all working

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import json
 import mysql.connector
 import login
@@ -9,25 +9,40 @@ app = Flask(__name__)
 # Map Route (Index)
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # -------- Google Map View --------
-    # Connect to the database & select static bike information
-    conn = mysql.connector.connect(
-        host=login.dbHost,
-        user=login.dbUser,
-        password=login.dbPassword,
-        database=login.dbDatabase
-    )
-    cursor = conn.cursor()
-    cursor.execute('SELECT position_lat, position_lng, name, number FROM static')
-    data = cursor.fetchall()
+    try:
+        # -------- Google Map View --------
+        # Connect to the database & select static bike information
+        conn = mysql.connector.connect(
+            host=login.dbHost,
+            user=login.dbUser,
+            password=login.dbPassword,
+            database=login.dbDatabase
+        )
+        cursor = conn.cursor()
+        cursor.execute('SELECT position_lat, position_lng, name, number FROM static')
+        data = cursor.fetchall()
 
-    return render_template('index.html', data=json.dumps(data))
+        # Connect to JCDecaux API for live station data: For marker hover
+        r = requests.get(login.jcdUri, params={'contract':login.jcdName,
+                                        'apiKey': login.jcdKey})
+
+        if r.status_code == 200:
+            # If connection successful:
+            print('Connection to JCDecaux successful! Marker Station Data Incoming...')
+            live_station_data = r.json()
+
+        else:
+            print('Error connecting to JCDecaux', r.status_code)
+
+        return render_template('index.html', data=json.dumps(data), live_station_data=json.dumps(live_station_data))
+    
+    except Exception as e:
+        print('Error connecting to database:', e)
 
 # Detailed Information Display
 @app.route('/detailed', methods=['GET', 'POST'])
 def detailed():
     # ------ Deatiled Station Information ------- 
-
     # If POST request submitted:
     if request.method == 'POST':
         try:
@@ -51,7 +66,6 @@ def detailed():
                         number = station.get('number')
                         name = station.get('name')
                         address = station.get('address')
-                        station_capacity = station.get('bike_stands')
                         available_bike_stands = station.get('available_bike_stands')
                         available_bikes = station.get('available_bikes')
                         status = station.get('status')
@@ -93,51 +107,34 @@ def detailed():
         except Exception as e:
             print("Connection error:", e)
 
-# Live station data on hover
-@app.route('/hover', methods=['GET', 'POST'])
-def hover():
-    # ------ Station Information - Marker Hover ------- 
-
-    # If POST request submitted:
+# Route Planner
+@app.route('/route', methods=['GET', 'POST'])
+def route():
+    # -------- Cian Route Code ----------
     if request.method == 'POST':
-        try:
-        # Connect to JCDecaux API
-            r = requests.get(login.jcdUri, params={'contract':login.jcdName,
-                                            'apiKey': login.jcdKey})
+        # Get the start and end locations from the form
+        start = request.form['start']
+        end = request.form['end']
+        # Get the latitude and longitude of the start and end locations
+        #by using the Google Maps Geocoding API
+        apiKey='AIzaSyANu9D6AUdAajvwdweM-tkgx6CX1J9NdvQ'
+        url_start = f'https://maps.googleapis.com/maps/api/geocode/json?address={start}&key={apiKey}'
+        url_end = f'https://maps.googleapis.com/maps/api/geocode/json?address={end}&key={apiKey}'
+        #parse the urls
+        response_start = requests.get(url_start).json()
+        response_end = requests.get(url_end).json()
+        #get the lat and lng from the json data
+        # store variables and values in a dictionary and return the dictionary. We can then extract the values in JavaScript.
+        route_data = {
+            'start_lat': response_start['results'][0]['geometry']['location']['lat'],
+            'start_lng': response_start['results'][0]['geometry']['location']['lng'],
+            'end_lat': response_end['results'][0]['geometry']['location']['lat'],
+            'end_lng': response_end['results'][0]['geometry']['location']['lng']
+        }
 
-            if r.status_code == 200:
-                # If connection successful:
-                print('Connection to JCDecaux successful!')
-                data = r.json()
+        print('Route data to be returned:', route_data)
 
-                # Extract station number POST request: 
-                user_request = int(request.form['station_num'])
-                print('Extracted station number =', user_request)
-                
-                # For each station, extract the following data points:
-                for station in data:
-                    station_num = station.get('number')
-                    if station_num == user_request:
-                        available_bike_stands = station.get('available_bike_stands')
-                        available_bikes = station.get('available_bikes')
-                        break
-                
-                # Response HTML for POST request
-                text_response = f"<p>Bikes: {available_bikes} Stands: {available_bike_stands}</p>"
-                    
-                try:
-                    # Render the HTML with the extracted data points
-                    return text_response
-                except Exception as e:
-                    print('Error displaying web page. Error: ', e)
-                    
-            else:
-                # If connection to JCDecaux fails, render an error message
-                error_response = "<h4>Something went wrong. Please try again.</h4>"
-                return error_response
-
-        except Exception as e:
-            print("Connection error:", e)
+        return jsonify(route_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
